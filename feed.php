855c77e37a00035c98b8745b28c2ceaeef79778b
Skinkworks <skinkworks42@gmail.com>
2012-04-08 20:05:02 -0400

    Added date.

--- feed.php
+++ feed.php
@@ -3,7 +3,7 @@ include 'common.php';
 $res = mysql_query("SELECT Poster, Time, Data FROM Feed_entries ORDER BY Time DESC;");
 $i = 0;
 while ($row = mysql_fetch_assoc($res)){
-	echo("<div id='post".$i."' class='post'><div id='poster'>From ".$row['Poster']."</div>");
+	echo("<div id='post".$i."' class='post'><div id='poster'>From ".$row['Poster']." at ".date('l jS h:i:s A',$row['Time'])."</div>");
 	echo("<div id='content'>".$row['Data']."</div></div><br/>");
 	++$i;
 }
