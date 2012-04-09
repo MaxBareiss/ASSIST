7fccbddb2d83ca8bf567489747dd3cbd62af8305
Skinkworks <skinkworks42@gmail.com>
2012-04-08 20:28:39 -0400

    Fixed bug where poster wouldn't show up

--- AddInfo.php
+++ AddInfo.php
@@ -1,6 +1,6 @@
 <?php
 include 'common.php';
-$query = "INSERT INTO feed_entries (Poster,Time,Data) VALUES (".$_POST['poster'].",".time().",\"".$_POST['data']."\");";
+$query = "INSERT INTO feed_entries (Poster,Time,Data) VALUES (\"".$_POST['poster']."\",".time().",\"".$_POST['data']."\");";
 //echo($query);
 mysql_query($query);
 header("Location: /AddInfo.html?success=true")
