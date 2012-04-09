7a85f23312327ef7d1c6ecb5b0d487b91c200d4a
Skinkworks <skinkworks42@gmail.com>
2012-04-09 09:47:14 -0400

    Changed "Super" in name to "Science"

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <html>
 <head>
-<title>Aperture Super Scouting Information System Technology: ASSIST</title>
+<title>Aperture Science Scouting Information System Technology: ASSIST</title>
 <LINK REL=StyleSheet HREF="Styles.css" TYPE="text/css" MEDIA=screen>
 </head>
 <body>
-<h1>Welcome to Aperture Super Scouting Information System Technology: ASSIST</h1>
+<h1>Welcome to Aperture Science Scouting Information System Technology: ASSIST</h1>
 <a href="AddInfo.html">Goto Posting Page</a><br/>
 <a href="RoundSchedule.html">Goto Annotated Round Schedule</a>
 <script src="http://code.jquery.com/jquery-1.7.2.min.js"></script>
