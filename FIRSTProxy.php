8dab7599b29d639ee977111c9399201553dad169
Skinkworks <skinkworks42@gmail.com>
2012-04-12 06:50:05 -0300

    Changed event to Philly.

--- FIRSTProxy.php
+++ FIRSTProxy.php
@@ -1,3 +1,3 @@
 <?php
-echo(file_get_contents("http://www2.usfirst.org/2012comp/Events/NJF/matchresults.html"));
+echo(file_get_contents("http://www2.usfirst.org/2012comp/Events/PA/matchresults.html"));
 ?>
